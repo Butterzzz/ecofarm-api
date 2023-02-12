1fedef5e0d16808e6f9806096f6eccccfefdac23
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-13 01:05:17 +0900

    feat: add parser

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const router = require('./routes/index.js');
 const { PORT = 3003 } = process.env;
 const app = express();
 
+app.use(express.json()); // middleware (парсер данных)
 app.use('/catalog', router);
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+    console.log(`Сервер слушает порт ${PORT}`)
 });
