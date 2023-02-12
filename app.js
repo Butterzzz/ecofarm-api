e8d4daadae24cdb0db741c8d1bd4b0cfaad461f2
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-13 01:54:41 +0900

    feat: add CORS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('./middlewares/cors.js');
 const router = require('./routes/index.js');
 
 const { PORT = 3003 } = process.env;
 const app = express();
 
 app.use(express.json()); // middleware (парсер данных)
+app.use(cors);
 app.use('/catalog', router);
 
 app.listen(PORT, () => {
