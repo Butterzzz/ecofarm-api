73ae2c28d39b13a33b99d67a01e7e06334ed77bc
Grinchenko Sergei <butters.blaga@gmail.com>
2023-02-19 03:23:56 +0900

    feat: add a POST request handler to save order information

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,8 @@
 const Order = require('../models/order');
+const nodemailer = require('nodemailer');
+const { USER, PASS } = process.env;
 
+// Обработчик GET-запроса для получения информации о всех заказах
 module.exports.getOrders = (req, res, next) => {
   Order.find({})
     .then(order => res.send(order))
@@ -8,34 +11,58 @@ module.exports.getOrders = (req, res, next) => {
 
 // Обработчик POST-запроса для сохранения информации о заказе
 module.exports.createOrder = (req, res) => {
-  const products = req.body.products;
+  const { products } = req.body;
+  const totalPrice = products.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
 
-  // Здесь вы можете сохранить информацию о заказе в базе данных или отправить ее на почту, например.
-  // В этом примере мы просто выводим информацию о заказе в консоль.
-  console.log('Получен новый заказ:', products);
+  console.log('Получен новый заказ:', { products, totalPrice });
 
-  // Создание нового заказа на основе полученных данных
-  const newOrder = new Order({
-    products: products,
-  });
+  // Сохранение заказа в базу данных
+  const order = new Order({ products, totalPrice });
+  order.save()
+    .then(() => {
+      console.log('Заказ успешно сохранен в базе данных');
+    })
+    .catch((error) => {
+      console.error('Ошибка при сохранении заказа в базе данных', error);
+    });
 
-  // Сохранение заказа в базе данных
-  newOrder.save((err) => {
-    if (err) {
-      console.error('Ошибка при сохранении заказа', err);
-      res.status(500).send('Ошибка при сохранении заказа');
-    } else {
-      console.log('Заказ успешно создан:', newOrder);
-      res.send('Заказ успешно создан!');
+  // Отправка уведомления о заказе на email
+  // Создаем объект транспорта для отправки почты через SMTP
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.yandex.ru',
+    port: 465,
+    secure: true,
+    auth: {
+      user: USER,
+      pass: PASS,
     }
   });
 
+  // Опции отправки письма
+  const mailOptions = {
+    from: 'Butters-blg@yandex.ru',
+    to: 'butters.blaga@gmail.com',
+    subject: 'Новый заказ на сайте EcoFarm',
+    text: `Новый заказ на сумму ${totalPrice} руб.`,
+  };
 
-  // Order.create()
-  //   .then(newOrder => res.send(newOrder))
-  //   .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+  // Отправка письма
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Ошибка при отправке уведомления', error);
+    } else {
+      console.log('Уведомление успешно отправлено', info.response);
+    }
+  });
+
+  // Отправка ответа клиенту
+  res.json({ message: 'Заказ успешно обработан' });
+
 };
 
+// Обработчик DELETE-запроса
 module.exports.deleteOrder = (req, res) => {
   Order.findByIdAndRemove(req.params.cardId)
     .then(() => res.send({ message: 'Заказ успешно удален' }))
